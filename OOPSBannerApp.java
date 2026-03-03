4822eefb7fb7f9d6658402a48c347132faa44fa4
mp1924 <mp1924@srmist.edu.in>
2026-03-03 09:02:14 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,36 +1,24 @@
-public static void main(String[] args) {
-
-    feature/UC2-BannerPrint
-    System.out.println("  *****   *****   ******   *****  ");
-    System.out.println(" *     * *     *  *     *  *      ");
-    System.out.println(" *     * *     *  *     *  *      ");
-    System.out.println(" *     * *     *  ******   *****  ");
-    System.out.println(" *     * *     *  *            *  ");
-    System.out.println(" *     * *     *  *            *  ");
-    System.out.println("  *****   *****   *        *****  ");
-
-public class stringjoin{
-
+public class OOPSArray{
     public static void main(String[] args) {
-        renderBanner();
-    }
 
-    public static void renderBanner() {
+        String[] lines = {
+            " ***     ***     *****   ***** ",
+			
+            "*   *   *   *    *    *  *     ",
+			
+            "*   *   *   *    *    *  *     ",
+			
+            "*   *   *   *    *****   ***** ",
+			
+            "*   *   *   *    *           * ",
+			
+            "*   *   *   *    *           * ",
+			
+            " ***     ***     *       ***** "
+        };
 
-        String line1 = String.join(" ", "*****",   "*****",  "****",  "*****");
-
-        String line2 = String.join(" ", "*   *",   "*   *",  "*  *",  "*    ");
-
-        String line3 = String.join(" ", "*   *",   "*   *",  "****",  "*****");
-
-        String line4 = String.join(" ", "*   *",   "*   *",  "*   ",  "    *");
-
-        String line5 = String.join(" ", "*****",   "*****",  "*   ",  "*****");
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
